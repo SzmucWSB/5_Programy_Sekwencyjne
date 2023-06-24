101b2a25918e77556a0b1c5669e232b3afe8b31d
Szymon 145751 <chx99818@student.chorzow.wsb.pl>
2023-06-24 05:44:10 +0200

    1

--- 2_Liczby_Parzyste/2_Liczby_Parzyste.cs
+++ 2_Liczby_Parzyste/2_Liczby_Parzyste.cs
@@ -4,29 +4,49 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Podaj liczbę parzystych liczb do wylosowania (n)");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Metoda nr  1:");
         Random random = new Random();
-        int iloscLiczbParzystych = 10;
-        int licznikLosowan = 0;
-
-        Console.Write("Wersja 1: ");
-        for (int i = 0; i < iloscLiczbParzystych; i++)
+        int iloscLosowan1 = 0;
+        int iloscLiczb1 = 0;
+        while (iloscLiczb1 < n)
         {
-            int wylosowanaLiczba;
-            do
+            int liczba = random.Next(-8, 9);
+            iloscLosowan1++;
+            if (liczba % 2 == 0)
             {
-                wylosowanaLiczba = random.Next(-8, 9);
-                licznikLosowan++;
-            } while (wylosowanaLiczba % 2 != 0);
+                Console.Write(liczba);
+                iloscLiczb1++;
+                if (iloscLiczb1 < n)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("Ilość losowań (metoda nr  1): " + iloscLosowan1);
 
-            Console.Write(wylosowanaLiczba);
-
-            if (i < iloscLiczbParzystych - 1)
+        Console.WriteLine();
+        Console.WriteLine("Metoda nr  2:");
+        int iloscLosowan2 = 0;
+        int iloscLiczb2 = 0;
+        while (iloscLiczb2 < n)
+        {
+            int liczba = random.Next(-4, 5) * 2;
+            iloscLosowan2++;
+            Console.Write(liczba);
+            iloscLiczb2++;
+            if (iloscLiczb2 < n)
             {
                 Console.Write(", ");
             }
         }
-
         Console.WriteLine();
-        Console.WriteLine("Liczba wykonanych losowań: " + licznikLosowan);
+        Console.WriteLine("Ilość losowań (metoda nr 2): " + iloscLosowan2);
+
+        Console.ReadLine();
     }
 }
